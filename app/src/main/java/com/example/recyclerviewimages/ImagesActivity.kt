4b35953807f679947eb59ae5f7f390f3bc1983e1
Harshul Jain <harjain@expediagroup.com>
2021-05-27 15:32:15 +0530

    Added new activity and button to show the activity

---
+++ app/src/main/java/com/example/recyclerviewimages/ImagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.recyclerviewimages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_images)
+    }
+}
--- app/src/main/java/com/example/recyclerviewimages/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewimages/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.recyclerviewimages
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val showOptionsButton = findViewById<Button>(R.id.show_options_button)
+        showOptionsButton.setOnClickListener{showImages()}
     }
+
+    private fun showImages() {
+        val imagesActivity = Intent(this, ImagesActivity::class.java)
+        intent.putExtra("url", getString(R.string.url))
+        startActivity(imagesActivity)
+    }
+
 }
