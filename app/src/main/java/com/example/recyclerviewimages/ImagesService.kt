d1502dfc3ad9185dc648fb8cfda18cfc7b5017a2
Harshul Jain <harjain@expediagroup.com>
2021-05-28 10:04:14 +0530

    Added Retrofit for API call

---
+++ app/src/main/java/com/example/recyclerviewimages/ImagesService.kt
@@ -0,0 +1,33 @@
+package com.example.recyclerviewimages
+
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//https://hub.dummyapis.com/ImagesList?text=Test&noofimages=10&height=120&width=120
+const val BASE_URL = "https://hub.dummyapis.com/"
+
+interface ImagesInterface {
+
+    @GET("ImagesList?")
+    fun getImages(
+        @Query("text") text: String,
+        @Query("noofimages") noofimages: Int,
+        @Query("height") height: Int,
+        @Query("width")width: Int
+    ): Call<List<String> >
+}
+
+object ImagesService {
+    val imagesInstace: ImagesInterface
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        imagesInstace = retrofit.create(ImagesInterface::class.java)
+    }
+}
--- app/src/main/java/com/example/recyclerviewimages/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewimages/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.recyclerviewimages
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,13 +16,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val showOptionsButton = findViewById<Button>(R.id.show_options_button)
-        showOptionsButton.setOnClickListener{showImages()}
+        showOptionsButton.setOnClickListener { showImages() }
+        getImages()
     }
 
     private fun showImages() {
         val imagesActivity = Intent(this, ImagesActivity::class.java)
         intent.putExtra("url", getString(R.string.url))
         startActivity(imagesActivity)
+    }
+
+    private fun getImages() {
+        val imagesList = ImagesService.imagesInstace.getImages("test", 5,
+            120, 120)
+
+        imagesList.enqueue(object: Callback<List<String> > {
+            override fun onResponse(call: Call<List<String>>, response: Response<List<String>>) {
+                val images: String = response.body().toString()
+                val textView: TextView = findViewById(R.id.textView)
+                textView.text = images
+                Log.d("Harshul", images)
+            }
+
+            override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                Log.d("Harshul", "Error")
+            }
+        })
     }
 
 }
